9101cc685ee2d2c03fdfc3cd02104e9ac77db407
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-24 17:38:27 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
-# Importing necessary libraries
 from embedchain import App
+from embedchain.config import AppConfig, AddConfig, ChunkerConfig, LlmConfig
 import streamlit as st
+from string import Template
 
 # Defining the app function
 def main():
 
-    st.title("Naval Chat Bot")
+    st.title("Chat Bot")
 
-    # Initializing the Embedchain App
-    naval_chat_bot = App()
+    # Configurations for Embedchain App
+    app_config = AppConfig(log_level="DEBUG")
+    chat_bot = App(config=app_config)
 
-    # Adding online resources
-    naval_chat_bot.add("https://www.youtube.com/watch?v=3qHkcs3kG44")
-    naval_chat_bot.add("https://navalmanack.s3.amazonaws.com/Eric-Jorgenson_The-Almanack-of-Naval-Ravikant_Final.pdf")
-    naval_chat_bot.add("https://nav.al/feedback")
-    naval_chat_bot.add("https://nav.al/agi")
+    # Custom Chunker Configuration
+    chunker_config = ChunkerConfig(chunk_size=300)  # Assuming text data type
+    add_config = AddConfig(chunker=chunker_config)
 
-    # Adding local resources
-    naval_chat_bot.add(("Who is Naval Ravikant?", "Naval Ravikant is an Indian-American entrepreneur and investor."))
+    # File Upload for Resources
+    uploaded_file = st.file_uploader("Choose a file to upload", type=["txt", "pdf", "docx", "mp4"])
+    
+    if uploaded_file is not None:
+        # Adding uploaded file with configurations
+        chat_bot.add(uploaded_file, config=add_config)
+        st.success("File uploaded successfully!")
+
+    # Custom Query Configuration
+    custom_query_template = Template(
+        """
+            Use the following information to respond to the human's query.
+            Context: $context
+
+            Keep the response brief. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+            Human: $query
+            Response:"""
+    )
+    llm_config = LlmConfig(template=custom_query_template, number_documents=5)
 
     # Getting user input
-    user_query = st.text_input("Ask a question:", value="What unique capacity does Naval argue humans possess when it comes to understanding explanations or concepts?")
+    user_query = st.text_input("Ask a question:")
 
     # Querying the Embedchain App
     if user_query:
-        answer = naval_chat_bot.query(user_query)
+        answer = chat_bot.query(user_query, config=llm_config)
         st.write(f"Answer: {answer}")
 
 # Running the main function
